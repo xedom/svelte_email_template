04e7a906e02f055345960e4209789f2d92f6e219
Kaj Rusilowski <xedom08@gmail.com>
2024-04-19 22:32:01 +0200

    update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,50 @@
 	import { templates, type TemplatesKey } from '$lib/templates';
 	import { keysOf, valuesOf } from '$lib/utils';
 	import Copy from '$lib/icon/Copy.svelte';
+	import Notebook from '$lib/icon/Notebook.svelte';
 
 	let current = valuesOf(templates)[0];
+	let currentText = '';
 
 	const setCurrent = (name: TemplatesKey) => (current = templates[name]);
 	const onCopy = async (name: TemplatesKey) => {
-		const template = await fetch(`api/${name}`).then((res) => res.text());
+		const template = await fetch(`api/${name}/text`).then((res) => res.text());
+		currentText = template;
 		await navigator.clipboard.writeText(template);
 	};
 </script>
 
-<div class="flex bg-slate-400 h-full gap-1 p-1">
+<div class="flex h-full gap-2 bg-neutral-950 p-2">
 	<nav class="flex flex-col gap-1">
 		{#each keysOf(templates) as name}
-			<div class="flex gap-1">
-				<button class="rounded flex-1 p-1 px-4 bg-neutral-800 text-white" on:click={() => setCurrent(name)}>
-					{name}
+			<div class="flex">
+				<button
+					class="group flex-1 rounded-s bg-neutral-800 p-1 px-4 text-white transition-all duration-300 hover:bg-neutral-900"
+					on:click={() => setCurrent(name)}
+				>
+					<span class="w-full text-white">{name}</span>
 				</button>
-				<a class="rounded p-1 px-3 bg-neutral-800 text-white" href="api/{name}" target="_blank">{'>'}</a>
-				<button class="rounded p-2 bg-neutral-800 text-white group" on:click={() => onCopy(name)}>
-					<Copy fill="#ffffff" class="size-4 text-white group-hover:opacity-70 transition-opacity duration-300" />
+				<a
+					class="group bg-neutral-800 p-2 text-white transition-all duration-300 hover:bg-neutral-900"
+					href="api/{name}"
+					target="_blank"
+				>
+					<Notebook fill="#ffffff" class="size-4 text-white" />
+				</a>
+				<button
+					class="group rounded-e bg-neutral-800 p-2 text-white transition-all duration-300 hover:bg-neutral-900"
+					on:click={() => onCopy(name)}
+				>
+					<Copy fill="#ffffff" class="size-4 text-white" />
 				</button>
 			</div>
 		{/each}
 	</nav>
 
-	<main class="flex-1 bg-neutral-800 rounded overflow-auto">
+	<main class="flex-1 overflow-auto rounded bg-neutral-800">
 		<svelte:component this={current} />
 	</main>
+	<!-- <main class="flex-1 bg-neutral-800 rounded overflow-auto">
+		<iframe srcdoc={currentText} title="site"></iframe>
+	</main> -->
 </div>
